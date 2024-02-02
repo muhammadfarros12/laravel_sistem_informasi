b9e923aea23289197bc13cf9a022473e62ff9d83
ammaar <muhammadfarroos12@gmail.com>
2024-02-03 06:11:45 +0700

    sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\NewsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/news', [NewsController::class, 'index']);
-Route::get('/news/{id}', [NewsController::class, 'show']);
+Route::get('/news', [NewsController::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/news/{id}', [NewsController::class, 'show'])->middleware(['auth:sanctum']);
+
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware(['auth:sanctum']);
+Route::get('/me', [AuthenticationController::class, 'me'])->middleware(['auth:sanctum']);
